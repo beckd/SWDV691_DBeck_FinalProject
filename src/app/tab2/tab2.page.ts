5350c329217166cb39fdc81552b768dd44090bb0
beckd <dbeck2@outlook.com>
2019-08-20 09:24:10 -0700

    Adding content for tab 2 About page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,9 @@ export class Tab2Page {
 
   constructor() {}
 
+  gameName = "Bread Knight by David Beck";
+  chapter = "Final Project for the SWDV 691 Capstone Course";
+  title = "A Faithful Update from a SWDV 600 Assignment";
+  story = "I wanted to revisit a previous assignment from SWDV 600, were we created text based adventure game in Python. I wanted to apply the skills I learned since then to that game and fully deploy it. ";
+  info = "This game was built on a MINE stack (MongoDB, Ionic, Node.js, Express)"
 }
