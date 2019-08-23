6e51b49845d61cd608305a979c4e7184ad79fd89
beckd <dbeck2@outlook.com>
2019-08-23 11:26:52 -0700

    Code clean up

--- src/app/villain.service.ts
+++ src/app/villain.service.ts
@@ -17,7 +17,7 @@ export class VillainService {
   }
 
   // Villain #1 - A Coin Flip
-  async villain_1_Story1(user) {
+  async villain_1_Story1() {
     const alert = await this.alertController.create({
       header: "Out of the corner of your eye you see a shadow in the shape of a small impish person.",
       subHeader: "You shrug it off and are about to re-embark on your journey, when suddenly out of that shadow yellow eyes appear.",
@@ -25,15 +25,15 @@ export class VillainService {
       buttons: [
         {
           text: 'Continue',
-          handler: (user) => {
-            this.villain_1_Story1_2(user);
+          handler: () => {
+            this.villain_1_Story1_2();
           }
         }
       ]
     });
     await alert.present();
   }
-  async villain_1_Story1_2(user) {
+  async villain_1_Story1_2() {
     const alert = await this.alertController.create({
       header: "The group surrounds you. The leader with the yellow eyes speaks in a low authoritative tone.",
       subHeader: "'You've wondered into the wrong part of town friend. You have two choices on how to proceed.'",
@@ -41,21 +41,21 @@ export class VillainService {
       buttons: [
         {
           text: 'Pay 10 Pickle Chips',
-          handler: (user) => {
-            this.villain_1_Story_Pay(user);
+          handler: () => {
+            this.villain_1_Story_Pay();
           }
         },
         {
           text: 'Flip',
-          handler: (user => {
-            this.villain_1_Story_Flip(user)
-          })
+          handler: () => {
+            this.villain_1_Story_Flip()
+          }
         }
       ]
     });
     await alert.present();
   }
-  async villain_1_Story_Pay(user) {
+  async villain_1_Story_Pay() {
     const alert = await this.alertController.create({
       header: "You take a look at the cadre of small brutes surrounding you and think it best to just pay and be on your way.",
       subHeader: "You hand over your Pickle Chips sheepishly and watch the group slink back into the shadows.",
@@ -80,7 +80,7 @@ export class VillainService {
     await alert.present();
   }   
 
-  async villain_1_Story_Flip(user) {
+  async villain_1_Story_Flip() {
     var coinFlip = Math.floor(Math.random() * 2)
     var heads = 0
     var tails = 1
@@ -92,9 +92,9 @@ export class VillainService {
           text: 'Tails',
           handler: () => {
             if (coinFlip == tails) {
-              this.villain_1_Flip_Win(user)
+              this.villain_1_Flip_Win()
             } else {
-              this.villain_1_Flip_Lose(user)
+              this.villain_1_Flip_Lose()
             }
           }
         },
@@ -102,9 +102,9 @@ export class VillainService {
           text: 'Heads',
           handler: () => {
             if (coinFlip == heads) {
-              this.villain_1_Flip_Win(user)
+              this.villain_1_Flip_Win()
             } else {
-              this.villain_1_Flip_Lose(user)
+              this.villain_1_Flip_Lose()
             }
           }
         }
@@ -113,7 +113,7 @@ export class VillainService {
     await alert.present();
   }
 
-  async villain_1_Flip_Win(user) {
+  async villain_1_Flip_Win() {
     const alert = await this.alertController.create ({
       header: "You've won!",
       subHeader: "'Drat!' the brute exclaims. 'Should've used a rigged coin! But a win is a win.' He tosses 5 Pickles Chips at your feet as the group retreats back into the shadows.",
@@ -138,7 +138,7 @@ export class VillainService {
     await alert.present();
   }
 
-  async villain_1_Flip_Lose(user) {
+  async villain_1_Flip_Lose() {
     const alert = await this.alertController.create({
       header: "You've lost!",
       subHeader: "'HAHA!' the brute exclaims. 'I'm unbeatable! Hand over my winnings!' You toss 10 Pickle Chips toward him and run as fast as you can till their laughter is only faintly heard.",
@@ -166,7 +166,7 @@ export class VillainService {
 
 // Villain #2 - Guess the Number
 
-  async villain_2_Story1(user) {
+  async villain_2_Story1() {
     const alert = await this.alertController.create({
       header: "You take step forward on a patch of mossy ground when you suddenly hear a loud SNAP! Followed by excrutiating pain shooting up your leg.",
       subHeader: "You look down to see your foot caught in a snare, sharp barbs dug into your ankle. Warm deep red blood is trickling down onto your shoe.",
@@ -174,15 +174,15 @@ export class VillainService {
       buttons: [
         {
           text: 'Continue',
-          handler: (user) => {
-            this.villain_2_Story1_2(user);
+          handler: () => {
+            this.villain_2_Story1_2();
           }
         }
       ]
     });
     await alert.present();
   }
-  async villain_2_Story1_2(user) {
+  async villain_2_Story1_2() {
     var guessNumber = Math.floor((Math.random() * 10) +1)
     console.log('guessNumber is ', guessNumber)
     const alert = await this.alertController.create({
@@ -225,7 +225,7 @@ export class VillainService {
       buttons: [
         {
           text: 'Take A Guess',
-          handler: (user) => {
+          handler: () => {
             if (guessNumber == name) {
               this.villain_2_Story_Win(guessNumber);
             } else {
@@ -250,7 +250,7 @@ export class VillainService {
       buttons: [
         {
           text: 'Take A Guess',
-          handler: (user) => {
+          handler: () => {
             if (guessNumber == name) {
               this.villain_2_Story_Win(guessNumber);
             } else {
@@ -270,7 +270,7 @@ export class VillainService {
       buttons: [
         {
           text: 'Continue',
-          handler: (user) => {
+          handler: () => {
             this.navCtrl.navigateForward('page8');
           }
         }
@@ -286,7 +286,7 @@ export class VillainService {
       buttons: [
         {
           text: 'Continue',
-          handler: (user) => {
+          handler: () => {
             this.userService.updateUser(this.user['_id'], {currency: -15, health: -10 }).subscribe(updateUser => {
               this.userService.statCheck(this.user['_id']).subscribe(statResult => {
                 this.userService.user = updateUser;
@@ -302,7 +302,7 @@ export class VillainService {
 
 // Villain #3 - Three Riddles
 
-  async villain_3_Story1(user) {
+  async villain_3_Story1() {
     const alert = await this.alertController.create({
       header: "A large Ogre steps out onto the path, blocking your way. He hasn't seen you yet.",
       subHeader: "You duck behind a tree. 'Is there any way around him?' you think to yourself. You quickly realize there is not.",
@@ -310,15 +310,15 @@ export class VillainService {
       buttons: [
         {
           text: 'Continue',
-          handler: (user) => {
-            this.villain_3_Story1_2(user);
+          handler: () => {
+            this.villain_3_Story1_2();
           }
         }
       ]
     });
     await alert.present();
   }
-  async villain_3_Story1_2(user) {
+  async villain_3_Story1_2() {
     const alert = await this.alertController.create({
       header: "'Ahem.' You clear your throat loudly. The Ogre stirs. 'Excuse me, would you mind terribly moving to one side so I may pass?'",
       subHeader: "The Ogre rolls his eyes. 'You see I'm on a great quest and must be on my way.' you continue.",
@@ -326,102 +326,102 @@ export class VillainService {
       buttons: [
         {
           text: 'Continue to First Riddle',
-          handler: (user) => {
-            this.villain_3_Story_Riddle1(user);
+          handler: () => {
+            this.villain_3_Story_Riddle1();
           }
         }
       ]
     });
     await alert.present();
   }
-  async villain_3_Story_Riddle1(user) {
+  async villain_3_Story_Riddle1() {
     const alert = await this.alertController.create({
       header: "'Riddle the first: Give me food, and I will live. Give me water, and I will die. What am I?'",
       buttons: [
         {
           text: 'Fire',
-          handler: (user) => {
-            this.villain_3_Story_Riddle2(user)
+          handler: () => {
+            this.villain_3_Story_Riddle2()
           }
         },
         {
           text: 'Gremlin',
-          handler: (user) => {
-            this.villain_3_Story_Lose(user)
+          handler: () => {
+            this.villain_3_Story_Lose()
           }
         },
         {
           text: 'Chinchilla',
-          handler: (user) => {
-            this.villain_3_Story_Lose(user)
+          handler: () => {
+            this.villain_3_Story_Lose()
           }
         }
       ]
     });
     await alert.present();
   }
-  async villain_3_Story_Riddle2(user) {
+  async villain_3_Story_Riddle2() {
     const alert = await this.alertController.create({
       header: "Impossible! Gah! You didn't look smart enough to get that one. On to the next one!",
       subHeader: "'Riddle the second: Take off my skin, I won't cry, but you will. What am I?'",
       buttons: [
         {
           text: 'Banana',
-          handler: (user) => {
-            this.villain_3_Story_Lose(user)
+          handler: () => {
+            this.villain_3_Story_Lose()
           }
         },
         {
           text: 'Orange',
-          handler: (user) => {
-            this.villain_3_Story_Lose(user)
+          handler: () => {
+            this.villain_3_Story_Lose()
           }
         },
         {
           text: 'Onion',
-          handler: (user) => {
-            this.villain_3_Story_Riddle3(user)
+          handler: () => {
+            this.villain_3_Story_Riddle3()
           }
         }
       ]
     });
     await alert.present();
   }
-  async villain_3_Story_Riddle3(user) {
+  async villain_3_Story_Riddle3() {
     const alert = await this.alertController.create({
       header: "Inconceivable! Only the best of the best get that one right! Onto the last riddle!",
       subHeader: "'Riddle the third and final!: If there are three apples and you take two away, how many apples do you have?'",
       buttons: [
         {
           text: '1',
-          handler: (user) => {
-            this.villain_3_Story_Lose(user)
+          handler: () => {
+            this.villain_3_Story_Lose()
           }
         },
         {
           text: '2',
-          handler: (user) => {
-            this.villain_3_Story_Win(user)
+          handler: () => {
+            this.villain_3_Story_Win()
           }
         },
         {
           text: '3',
-          handler: (user) => {
-            this.villain_3_Story_Lose(user)
+          handler: () => {
+            this.villain_3_Story_Lose()
           }
         }
       ]
     });
     await alert.present();
   }
-  async villain_3_Story_Win(user) {
+  async villain_3_Story_Win() {
     const alert = await this.alertController.create({
       header: "'Blast! You've done it!' the Ogre replies stepping aside, allowing you onto the bridge.",
       subHeader: "'The Sandwich awaits you inside.' Your heart races as you dash across the bridge and into the gapping mouth of the dark cave.",
       buttons: [
         {
           text: 'Continue to Final Stage!',
-          handler: (user) => {
+          handler: () => {
             this.navCtrl.navigateForward('final');
           }
         }
@@ -429,7 +429,7 @@ export class VillainService {
     });
     await alert.present();
   }
-  async villain_3_Story_Lose(user) {
+  async villain_3_Story_Lose() {
     const alert = await this.alertController.create({
       header: "'I knew you didn't have it in you. Seldom do.' the Ogre said with a satisfied grin.",
       subHeader: "'Head home and forget about the Sandwich. It wasn't meant for you.'",
@@ -437,7 +437,7 @@ export class VillainService {
       buttons: [
         {
           text: 'Continue',
-          handler: (user) => {
+          handler: () => {
             this.navCtrl.navigateForward('lose');
           }
         }
